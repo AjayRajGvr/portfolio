e659d9331c2557b8c8e7f4dbc0a77f0b2ba200c8
Ajay Raj kollath <ajayrajkollath@Ajays-MacBook-Air.local>
2026-05-06 22:48:33 -0700

    feat: changed baseUrl

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,7 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 
-const site = process.env.PUBLIC_SITE_URL ?? "https://ajayraj.ca";
-const base = process.env.PUBLIC_BASE_PATH ?? "/";
+const site = "https://ajayraj.ca";
 
 export default defineConfig({
   site,
